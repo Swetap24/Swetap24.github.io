a5918c66a2b3272f43c09ff4d615fce186e08a40
Sweta Priyadarshi <52484860+Swetap24@users.noreply.github.com>
2024-02-25 23:32:26 -0500

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -1,3 +1,8 @@
+res.setHeader('Access-Control-Allow-Origin', '*'); // Or restrict to your domain
+res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+
 async function sendToChatGPT() {
     const userInput = document.getElementById('userInput').value;
     const responseContainer = document.getElementById('response');
